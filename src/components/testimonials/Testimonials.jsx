1e711801f97b0aa117c7b307daa1c8d7c0de553a
EduOceans <150608334+EduOceans@users.noreply.github.com>
2024-07-08 11:58:15 +0100

    updating testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -15,7 +15,7 @@ export function Testimonials () {
                 <input type="radio" name="slider" id="slide4" checked></input>
                 <div id="slides">
                     <div id="overflow">
-                    <img src={quoteImg} className="quote-sign"></img>
+                    <img src={quoteImg} className="quote-sign" alt="quote"></img>
                         <div className="inner">
                             <div className="slide slide_1">
                                 <div className="slide-content">
@@ -64,84 +64,3 @@ export function Testimonials () {
         </div>
     )
 }
-
-
-
-{/*         
-            <section className="container">
-                <div className="testimonial">
-                    <div className="testi-content">
-                        <div className="slide">
-                            <img src="src/assets/quote.png" className="image" alt="quote"></img>
-                            <div id="slide-1">
-                            <p className="quote">
-                                I have to say how impressed I was to see you build upon the skills you acquired thus far to build something so beautiful and unique!
-                            </p>
-                            
-                            <div className="quote-details">
-                                <span className="quote-name">JC - </span>
-                                <span className="quote">Central Grader</span>
-                            </div>
-                            </div>
-                        </div>
-
-                        <div className="slide">
-                            <img src="src/assets/quote.png" className="image" alt="quote"></img>
-                            <div id="slide-2">
-                                <p className="quote">
-                                I wanted to take a moment to acknowledge your outstanding work so far. your dedication to excellence is evident in your work.
-                                </p>
-                                
-                                <div className="quote-details">
-                                    <span className="quote-name">JC - </span>
-                                    <span className="quote">Central Grader</span>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="slide">
-                            <img src="src/assets/quote.png" className="image" alt="quote"></img>
-                            <div id="slide-3">
-                            <p className="quote">
-                            You have done a fantastic job and should be proud of your work Eduardo! Keep up the awesome job!
-                            </p>
-                                
-                                <div className="quote-details">
-                                    <span className="quote-name">-Chris Baird - </span>
-                                    <span className="quote">Central Grader</span>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="slide">
-                            <img src="src/assets/quote.png" className="image" alt="quote"></img>
-                            <div id="slide-4">
-                            <p className="quote">
-                                I'm truly impressed, Eduardo, by how you've built upon your skills to create something so unique and visually appealing. Your clean coding practices and the deployment of a comprehensive repository are commendable achievements. Keep up the great work and maintain this exceptional momentum!
-                            </p>
-                            
-                            <div className="quote-details">
-                                <span className="quote-name">Robin Clarke - </span>
-                                <span className="quote">Central Grader</span>
-                            </div>
-                            </div>
-                        </div>
-
-                        <div className="slide">
-                            <img src="src/assets/quote.png" className="image" alt="quote"></img>
-                            <div id="slide-5">
-                            <p className="quote">
-                                The UI is polished and the page is very responsive. I like your ideas for future development and I think this website serves as a great portfolio piece Nice job!
-                            </p>
-                            
-                            <div className="quote-details">
-                                <span className="quote-name">Kayvon Kazemini - </span>
-                                <span className="quote">Central Grader</span>
-                            </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="slider-nav">
-                    </div>
-                </div>
-            </section> */}
